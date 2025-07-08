b2c374682af7fa46111faf18831dfafcdc08b1a1
Yujiang Wu <wuyujiangacademy@yahoo.com>
2025-07-07 23:40:23 -0400

    Bugfix: correct setup.py and generate requirements.txt

--- app/main.py
+++ app/main.py
@@ -8,6 +8,10 @@ app = FastAPI(
   version="1.0.0"
 )
 
+@app.get("/")
+def read_root():
+  return {"message": "Hello, this is your RAG assistant backend is alive!"}
+
 app.include_router(api_router.router)
 
 
--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
 from setuptools import setup, find_packages
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
 
 setup(
     name="rag_assistant",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
+    python_requires=">=3.8",
     install_requires=[
+        "packaging==24.0",
         "llama-cpp-python>=0.2.24",
-        "openai",
-        "langchain",
-        "langchain-community",
-        "faiss-cpu",
-        "pypdf",
-        "python-dotenv",
-        "tiktoken",
+        "openai==1.93.1",
+        "langchain==0.2.17",
+        "langchain-community==0.2.19",
+        "langchain-openai==0.1.25",
+        "tokenizers>=0.19.1,<0.20",
+        "langchain_huggingface",
+        "faiss-cpu==1.8.0.post1",
+        "pypdf==5.7.0",
+        "python-dotenv==1.0.1",
+        "tiktoken==0.7.0",
         "fastapi>=0.100",
         "uvicorn>=0.22",
         "gunicorn",
@@ -21,7 +28,7 @@ setup(
     ],
     author="Yujiang Wu",
     description="A RAG-based personal assistant using LangChain and OpenAI",
-    long_description=open("README.md").read(),
+    long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
         "Programming Language :: Python :: 3",
